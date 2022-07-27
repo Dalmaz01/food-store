1416dee29bcef1192a0956dcc233f15ed65602ed
Almazon <d.almaz0140@gmail.com>
2022-07-27 18:44:47 +0600

    Models Category, Rating, Foods are added.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='category')
+
+    def __str__(self):
+        return self.name
+
+
+class Ratings(models.Model):
+    number = models.IntegerField(default=0, verbose_name='number of rate')
+
+    def __str__(self):
+        return f"{self.number} star"
+
+
+class Foods(models.Model):
+    name = models.CharField(max_length=100, verbose_name='name of food')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.IntegerField(default=0, verbose_name='price of food')
+    description = models.TextField(verbose_name='description')
+    rating = models.ForeignKey(Ratings, on_delete=models.PROTECT)
+    image = models.ImageField(verbose_name='Image of food', default=None, null=None)
+
+    def __str__(self):
+        return self.name
+
+
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from . import models
 
 
 def home_page(request):
-    return render(request, 'main/home.html', {})
+    all_foods = models.Foods.objects.all()
+    return render(request, 'main/home.html', {'all_foods': all_foods})
