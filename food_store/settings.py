b8af5c9ae5798f897f414895c363cd00533761fa
Almazon <d.almaz0140@gmail.com>
2022-08-12 18:09:40 +0600

    URL is rebased

--- food_store/settings.py
+++ food_store/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-up+9fv0tas$2tr3a$^6(cno&6ey(3q%*n)4f07rzn-5-6y!&-s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
--- food_store/urls.py
+++ food_store/urls.py
@@ -22,6 +22,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('main/', include('main.urls')),
     path('manager/', include('manager.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
